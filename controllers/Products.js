aaef9b85703c5d272488c7eb24a9f8e59d076a62
TMillsap04 <tmmillsap@student.fullsail.edu>
2022-05-13 22:17:32 -0500

    Updated files

--- controllers/Products.js
+++ controllers/Products.js
@@ -1,31 +1,31 @@
-const Products = require('../models/Products');
+const Products = require("../models/Products");
 
 const index = (req, res) => {
     const products = Products.all();
-    res.json(products);
-}
+    res.render('views/products/index', { products });
+};
 
 const form = ( req, res) => {
-    res.send('Product.form');
-}
+    res.send("Product.form");
+};
 
 const show = ( req, res) => {
     const product = Products.find(req.params.id);
-    res.json(product);
-}
+    res.render('views/products/show', { product });
+};
 
 const create = ( req, res) => {
     const product = Products.create(req.body);
     res.json(product);
-}
+};
 
 const update = ( req, res) => {
     const product = Products.update(req.params.id, req.body);
     res.json(product);
-}
+};
 
 const remove = (req,res) => {
-    const products = Products.remove(req.params.id);
-    res.json(products);
-}
+    const product = Products.remove(req.params.id);
+    res.json(product);
+};
 module.exports = { index, form, show, create, update, remove };
--- models/products.js
+++ models/products.js
@@ -1,32 +1,33 @@
 let products =[
-   {id: 1,
-    "slug": "nike-shoe",
-    "label": "Nike Shoe"} 
-]
+    {id: "1", slug: "nike-shoe", title: "Nike Shoe"},
+    {id: "1", slug: "nike-shoe", title: "Nike Shoe"},
+    {id: "1", slug: "nike-shoe", title: "Nike Shoe"},
+    {id: "1", slug: "nike-shoe", title: "Nike Shoe"},
+];
 
 const all =  () => {
-    return products
-}
+    return products;
+};
 
 const find = (id) => {
-    return products.find(p => p.id === id)
-}
+    return products.find((p) => p.id === id);
+};
 
 const create =(product) => {
-    products.push(product)
-    return products
-}
+    products.push(product);
+    return products;
+};
 
 const update = (id, product) => {
-    products.map((p) => {
-        return (p.id === id) ? product : p
-    })
-    return products
-}
+    products = products.map((p) => {
+        return p.id === id ? product : p;
+    });
+    return products;
+};
 
 const remove = (id) => {
-    products = product.filter(p => p.id !== id)
-    return products
-}
+    products = products.filter((p) => p.id !== id);
+    return products;
+};
 
-module.exports = { all, find, update, remove, create}
+module.exports = { all, find,create, update, remove};
